537b7b07c90a5728837a95eb9a419a5c3c5142bf
Yanina553 <larina.yanina@gmail.com>
2026-03-27 10:50:30 +0300

    Update ApiService.kt

--- app/src/main/java/com/example/mobileauthclient/ApiService.kt
+++ app/src/main/java/com/example/mobileauthclient/ApiService.kt
@@ -1,1 +1,16 @@
+package com.example.mobileauthclient
 
+import com.example.mobileauthclient.models.*
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ApiService {
+    @POST("/register")
+    suspend fun register(@Body request: RegisterRequest): Response<RegisterResponse>
+    
+    @POST("/login")
+    suspend fun login(@Body request: AuthRequest): Response<AuthResponse>
+    
+    @GET("/api/posts")
+    suspend fun getProtectedPosts(@Header("Authorization") token: String): Response<List<Post>>
+}
